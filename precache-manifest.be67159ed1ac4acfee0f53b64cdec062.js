36952b88996c268329fc88c1db14fabc2c21313e
leeya <leeyahav018@gmail.com>
2020-08-17 08:08:20 +0300

    Updates

---
+++ precache-manifest.be67159ed1ac4acfee0f53b64cdec062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c5b066a0446ab1e94bc8389f1e5879",
+    "url": "/currecnty-converter1/index.html"
+  },
+  {
+    "revision": "ec5643a05159f83487d1",
+    "url": "/currecnty-converter1/static/css/main.52d6a0cd.chunk.css"
+  },
+  {
+    "revision": "2c63c438288c84b5265d",
+    "url": "/currecnty-converter1/static/js/2.e02fc662.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currecnty-converter1/static/js/2.e02fc662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5643a05159f83487d1",
+    "url": "/currecnty-converter1/static/js/main.4a34088a.chunk.js"
+  },
+  {
+    "revision": "f18881a7485fe2645891",
+    "url": "/currecnty-converter1/static/js/runtime-main.e9e35656.js"
+  }
+]);
